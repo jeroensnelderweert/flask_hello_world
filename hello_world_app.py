2fdda2a39f1ea6076f91d11c32bc914cf7bea003
Jeroen Snelderweert <jeroen.snelderweert@codeinvest.nl>
2023-08-22 09:51:29 +0200

    Added function to calculate and display the week number

--- hello_world_app.py
+++ hello_world_app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
+def calculate_weeknumber():
+    date = datetime.datetime.now()
+    week_number = date.isocalendar().week
+    return f"Week number is: {week_number}"
+
+if __name__ == "__main__":
+    app.run()
